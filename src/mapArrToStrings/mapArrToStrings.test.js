26c790a38d77e1a08aeb94e6d4245e60f126140f
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-07-31 16:22:00 +0300

    Update test for mapArrToString function

--- src/mapArrToStrings/mapArrToStrings.test.js
+++ src/mapArrToStrings/mapArrToStrings.test.js
@@ -4,7 +4,13 @@ describe('mapArrToStrings', () => {
     test('Корректное значение', () => {
         expect(mapArrToStrings([1, 3, 5])).toEqual(['1', '3', '5']);
     })
-    test('Корректное значение 2', () => {
-        expect(mapArrToStrings(['1', 3, 5])).toEqual(['3', '5']);
+    test('Мешанина', () => {
+        expect(mapArrToStrings([1, 3, 5, null, undefined, 'sdsd'])).toEqual(['1', '3', '5']);
+    })
+    test('Пустое значение', () => {
+        expect(mapArrToStrings([])).toEqual([]);
+    })
+    test('Отрицание', () => {
+        expect(mapArrToStrings([1, 3, 5])).not.toEqual([1, 2, 3, 4]);
     })
 })
